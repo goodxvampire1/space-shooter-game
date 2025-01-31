6769916bf151ba3660749e517841c6b743af4d3d
goodxvampire1 <goodxvampire@gmail.com>
2025-01-31 23:13:36 +0600

    game.js

--- game.js
+++ game.js
@@ -19,6 +19,7 @@ let bullets;
 let enemies;
 let score = 0;
 let scoreText;
+let gameOverText;
 
 const game = new Phaser.Game(config);
 
@@ -26,18 +27,22 @@ function preload() {
     this.load.image('player', 'https://i.imgur.com/uLXtWtb.png'); // স্পেসশিপ
     this.load.image('bullet', 'https://i.imgur.com/3X7vU53.png'); // বুলেট
     this.load.image('enemy', 'https://i.imgur.com/CMtAQys.png'); // শত্রু
+    this.load.spritesheet('explosion', 'https://i.imgur.com/kjA7pDg.png', { frameWidth: 64, frameHeight: 64 }); // এক্সপ্লোসন অ্যানিমেশন
 }
 
 function create() {
+    // প্লেয়ার তৈরি
     player = this.physics.add.sprite(400, 500, 'player').setScale(0.5);
     player.setCollideWorldBounds(true);
 
     bullets = this.physics.add.group();
     enemies = this.physics.add.group();
 
+    // কিপ্যাড কন্ট্রোল
     cursors = this.input.keyboard.createCursorKeys();
     this.input.keyboard.on('keydown-SPACE', shootBullet, this);
 
+    // এনেমি স্পাওন
     this.time.addEvent({
         delay: 1000,
         callback: spawnEnemy,
@@ -45,10 +50,23 @@ function create() {
         loop: true
     });
 
+    // স্কোর টেক্সট
     scoreText = this.add.text(10, 10, 'Score: 0', { fontSize: '20px', fill: '#fff' });
+
+    // গেমওভার টেক্সট
+    gameOverText = this.add.text(300, 250, '', { fontSize: '40px', fill: '#fff' });
+
+    // প্লেয়ার এবং শত্রুদের সংঘর্ষ
+    this.physics.add.collider(player, enemies, gameOver, null, this);
+
+    // বুলেট এবং এনেমির সংঘর্ষ
+    this.physics.add.collider(bullets, enemies, destroyEnemy, null, this);
 }
 
 function update() {
+    if (gameOverText.visible) return; // গেমওভার হলে আর কিছু আপডেট হবে না
+
+    // প্লেয়ার মুভমেন্ট
     if (cursors.left.isDown) {
         player.setVelocityX(-300);
     } else if (cursors.right.isDown) {
@@ -69,8 +87,7 @@ function spawnEnemy() {
     let xPosition = Phaser.Math.Between(50, 750);
     let enemy = enemies.create(xPosition, 50, 'enemy');
     enemy.setVelocityY(150);
-
-    this.physics.add.collider(bullets, enemy, destroyEnemy, null, this);
+    enemy.setOutOfBoundsKill(true); // শত্রু স্ক্রীন থেকে বের হলে মারা যাবে
 }
 
 function destroyEnemy(bullet, enemy) {
@@ -78,4 +95,15 @@ function destroyEnemy(bullet, enemy) {
     enemy.destroy();
     score += 10;
     scoreText.setText('Score: ' + score);
-                                     }
+}
+
+function gameOver() {
+    // গেমওভার হলে প্লেয়ারকে রেড রঙে পরিণত করুন
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    this.physics.pause();
+
+    // গেমওভার টেক্সট
+    gameOverText.setText('Game Over! Final Score: ' + score);
+    gameOverText.visible = true;
+                                                }
